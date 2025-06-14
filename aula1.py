4958bdd1e3d27079dcde187069d6600e97aaf6a7
paulinoaps <silva.alexandrepaulino@gmail.com>
2025-06-13 21:50:07 -0300

    primeira aula

---
+++ aula1.py
@@ -0,0 +1,10 @@
+print("olá Mundo") 
+numero = 10
+numero2 = 20
+soma = numero + numero2 
+print("A soma de", numero,"mais", numero2, "é:", soma) 
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+soma2 = n1 + n2 
+print("A soma de", n1, "mais", n2, "é:", soma2)
